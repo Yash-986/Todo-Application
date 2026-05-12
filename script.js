2183fa2842448fc0e13d6237f3a351f24873aea6
Yash-968 <yashsehrawat756@gmail.com>
2026-05-12 20:12:16 +0530

    adding localStorage

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
 const addBtn = document.getElementById('addButton');
 const todoInputs = document.getElementById('todoInputs');
-const todoList = document.getElementById('todoList')
+const todoList = document.getElementById('todoList');
+const themeBtn = document.getElementById('themeBtn');
+
+window.addEventListener('load', loadTodos);
 
 addBtn.addEventListener('click', addTodo);
 
-function addTodo(){
+function addTodo() {
     const task = todoInputs.value.trim();
-    if (task === ""){
+
+    if (task === "") {
         alert('Please enter a task');
         return;
     }
 
-    const li = document.createElement('li');
-    li.innerHTML = `${task} <button class='delete-btn'> Delete </button>`
+    createTodo(task);
+    saveTodos();
 
-    const deleteBtn = li.querySelector('.delete-btn');
-
-    deleteBtn.addEventListener('click', () => {
-        li.remove();
-    });
-
-    todoList.appendChild(li);
     todoInputs.value = '';
 }
 
-const themeBtn = document.getElementById('themeBtn');
+function createTodo(task) {
+    const li = document.createElement('li');
+
+    const textSpan = document.createElement('span');
+    textSpan.textContent = task;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+        saveTodos();
+    });
+
+    li.appendChild(textSpan);
+    li.appendChild(deleteBtn);
+
+    todoList.appendChild(li);
+}
+
+function saveTodos() {
+    const todos = [];
+
+    document.querySelectorAll('#todoList li span').forEach((span) => {
+        todos.push(span.textContent);
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function loadTodos() {
+    const storedTodo = JSON.parse(localStorage.getItem('todos')) || [];
+
+    storedTodo.forEach((task) => {
+        createTodo(task);
+    });
+}
 
 themeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 
-    if (document.body.classList.contains('dark')){
+    if (document.body.classList.contains('dark')) {
         themeBtn.textContent = 'Light Mode';
     } else {
         themeBtn.textContent = 'Dark Mode';
