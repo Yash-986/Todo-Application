8c444238a959540988ea8b22fa6a7de27e18f11a
Yash-968 <yashsehrawat756@gmail.com>
2026-05-12 20:03:54 +0530

    update, add dark mode

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ function addTodo(){
     todoList.appendChild(li);
     todoInputs.value = '';
 }
+
+const themeBtn = document.getElementById('themeBtn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+
+    if (document.body.classList.contains('dark')){
+        themeBtn.textContent = 'Light Mode';
+    } else {
+        themeBtn.textContent = 'Dark Mode';
+    }
+});
